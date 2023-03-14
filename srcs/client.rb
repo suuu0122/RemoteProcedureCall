39071cacfe1da08c3f3e4675c04d6172766ff1db
suuu0122 <suuu0122lab@gmail.com>
2023-03-14 19:57:15 +0900

    fix to use class

--- srcs/client.rb
+++ srcs/client.rb
@@ -1,17 +1,26 @@
 require "json"
 require "socket"
 
-server_address = "socket_file"
-sock = UNIXSocket.new(server_address)
+class Client
+	def initialize(server_address)
+		@server_address = server_address
+	end
 
-json_file = File.open("../json/sample2.json", 'r')
-message_hash = JSON.load(json_file)
-json_file.close
+	def start
+		sock = UNIXSocket.new(@server_address)
 
-message_json = JSON.dump(message_hash)
-sock.send(message_json, 0)
+		json_file = File.open("../json/sample2.json", 'r')
+		message_hash = JSON.load(json_file)
+		json_file.close
+		
+		message_json = JSON.dump(message_hash)
+		sock.send(message_json, 0)
+		
+		puts "Server response: " + sock.recv(4096)
+		sock.close
+		puts "Closing socket"
+	end
+end
 
-puts sock.recv(4096)
-sock.close
-
-
+client = Client.new("socket_file")
+client.start
