389449f204160c6da75c1e96ef68447f213d5946
suuu0122 <suuu0122lab@gmail.com>
2023-03-14 19:49:56 +0900

    fix to use class

--- srcs/client.py
+++ srcs/client.py
@@ -2,31 +2,37 @@ import json
 import socket
 import sys
 
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-server_address = "socket_file"
+class Client:
+    def __init__(self, server_address):
+        self.server_address = server_address
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-try:
-    sock.connect(server_address)
-except socket.error as err:
-    print(err)
-    sys.exit(1)
+    def start(self):
+        try:
+            self.sock.connect(self.server_address)
+        except socket.error as err:
+            print(err)
+            sys.exit(1)
 
-while True:
-    try:
-        message = open("../json/sample1.json", 'r')
-        message_dict = json.load(message)
-        message_json = json.dumps(message_dict)
-        sock.sendall(message_json.encode("utf-8"))
-        message.close()
+        while True:
+            try:
+                message = open("../json/sample1.json", 'r')
+                message_dict = json.load(message)
+                message_json = json.dumps(message_dict)
+                self.sock.sendall(message_json.encode("utf-8"))
+                message.close()
 
-        response = sock.recv(4096).decode("utf-8")
-        if response:
-            print(f"Server response: {response}")
-            break
-        else:
-            break
-    except KeyboardInterrupt:
-        print("\nClosing socket")
-        sock.close()
-        sys.exit()
-        break
+                response = self.sock.recv(4096).decode("utf-8")
+                if response:
+                    print(f"Server response: {response}")
+                else:
+                    break
+            finally:
+                print("\nClosing socket")
+                self.sock.close()
+                sys.exit()
+                break
+
+if __name__ == '__main__':
+    client = Client("socket_file")
+    client.start()
